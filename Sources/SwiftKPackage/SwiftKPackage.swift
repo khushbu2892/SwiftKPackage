981c5b221d550785e9b01ee23c65955cb47c00fe
[khushbu2892] <khushbu.s@cearsinfotech.com>
2021-03-30 16:19:20 +0530

    Update

--- Sources/SwiftKPackage/SwiftKPackage.swift
+++ Sources/SwiftKPackage/SwiftKPackage.swift
@@ -1,15 +1,23 @@
 
 import Foundation
-
+import UIKit
 
 //struct SwiftKPackage {
 //    var text = "Hello, World!"
 //}
-struct Validations {
-    static func isValidEmailID(email:String) -> Bool {
-        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
-        
-        let emailPredicate = NSPredicate(format: "SELF MATCHED %@", emailRegEx)
-        return emailPredicate.evaluate(with: email)
-    }
+//public struct Validations {
+//   public static func isValidEmailID(email:String) -> Bool {
+//        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+//        
+//        let emailPredicate = NSPredicate(format: "SELF MATCHED %@", emailRegEx)
+//    
+//        return emailPredicate.evaluate(with: email)
+//    }
+//}
+extension String{
+    func isValidEmail() -> Bool {
+           // here, `try!` will always succeed because the pattern is valid
+           let regex = try! NSRegularExpression(pattern: "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$", options: .caseInsensitive)
+           return regex.firstMatch(in: self, options: [], range: NSRange(location: 0, length: count)) != nil
+       }
 }
