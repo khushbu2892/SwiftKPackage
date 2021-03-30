c9588073d20fa9e2845f269e9a9a13d24ba39669
[khushbu2892] <khushbu.s@cearsinfotech.com>
2021-03-30 15:35:29 +0530

    Update

--- Sources/SwiftKPackage/SwiftKPackage.swift
+++ Sources/SwiftKPackage/SwiftKPackage.swift
@@ -2,9 +2,9 @@
 import Foundation
 
 
-struct SwiftKPackage {
-    var text = "Hello, World!"
-}
+//struct SwiftKPackage {
+//    var text = "Hello, World!"
+//}
 struct Validations {
     static func isValidEmailID(email:String) -> Bool {
         let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
